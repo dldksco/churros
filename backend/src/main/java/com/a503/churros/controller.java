7f0113029e205cb68534f0e578eeca0b39111a5a
이안채 <dldksco@naver.com>
2023-03-10 11:09:11 +0900

    Feat:Backend Docker-compose and Dockerfile

---
+++ backend/src/main/java/com/a503/churros/controller.java
@@ -0,0 +1,17 @@
+package com.a503.churros;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+  @Value("${es.host}")
+  String h;
+
+  @GetMapping("/")
+  public String hi() {
+    return h;
+  }
+}
