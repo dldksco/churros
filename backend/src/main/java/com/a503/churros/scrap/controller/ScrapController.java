e8c2937550400703bd08fa92efcdc42be157ed73
이안채 <dldksco@naver.com>
2023-03-14 16:56:55 +0900

    Feat:Docker file

--- backend/src/main/java/com/a503/churros/scrap/controller/ScrapController.java
+++ backend/src/main/java/com/a503/churros/scrap/controller/ScrapController.java
@@ -15,7 +15,7 @@ public class ScrapController {
 
     @GetMapping("/")
     public ResponseEntity<?> getScrap(){
-        System.out.println("test2");
+        System.out.println("test3");
         return null;
     }
 }
