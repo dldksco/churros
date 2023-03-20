e667dbcf0679720f82ad92eb082c151d13b31920
KevRiver <kanggakky1214@gmail.com>
2023-03-20 17:42:59 +0900

    Feat: 사이드 바에서 스크랩 폴더 버튼을 눌렀을 때 드롭다운될 컴포넌트 작성 중

---
+++ frontend/churros/src/components/ScrapFolderSidebarListItem.js
@@ -0,0 +1,7 @@
+import SidebarItem from "./SidebarItem";
+
+const ScrapFolderSidebarListItem = ({title, scrapFolderIdx }) => {
+    return <div></div>
+}
+
+export default ScrapFolderSidebarListItem;
