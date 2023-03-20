8eda64db545e1ba1abd6b7c4434559cc9829c62e
KevRiver <kanggakky1214@gmail.com>
2023-03-20 17:41:49 +0900

    Feat: 메인 페이지 사이드바 작업 시작

--- frontend/churros/src/pages/MainPage.js
+++ frontend/churros/src/pages/MainPage.js
@@ -1,5 +1,19 @@
+import React, { Fragment } from "react";
+import { ReactDOM } from "react";
+import { IconContext } from "react-icons";
+import { FaHeart } from "react-icons/fa";
+import Sidebar from "../components/Sidebar";
+
 const MainPage = () => {
-  return <h1>여기는 메인 페이지입니다</h1>;
+  return (
+    <div className="w-screen h-screen flex flex-row justify-start bg-stone-50">
+      <Sidebar className={"w-64 h-full bg-stone-100"} />
+      <div className="flex flex-col justify-start w-full h-full">
+        Section
+        <FaHeart style={{color: "red"}} />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
