1635c817302f98ed03e586844b2b9e27d0eccb43
tjwnrhkd12 <tjwnrhkd12@naver.com>
2023-03-21 16:07:43 +0900

    FEAT : MyPage

--- backend/src/main/java/com/a503/churros/service/auth/AuthService.java
+++ backend/src/main/java/com/a503/churros/service/auth/AuthService.java
@@ -3,11 +3,13 @@ package com.a503.churros.service.auth;
 
 import com.a503.churros.dto.auth.request.SignInRequest;
 import com.a503.churros.dto.auth.request.SignUpRequest;
+import com.a503.churros.dto.auth.response.AuthResponse;
+import com.a503.churros.dto.auth.response.MessageResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
 
-    public ResponseEntity<?> signin(SignInRequest signInRequest);
-    public ResponseEntity<?> signup(SignUpRequest signUpRequest);
+    public AuthResponse signin(SignInRequest signInRequest);
+    public MessageResponse signup(SignUpRequest signUpRequest);
 
 }
