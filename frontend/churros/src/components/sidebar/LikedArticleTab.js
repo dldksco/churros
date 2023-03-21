2b270e00f36c5dfbeb6132d9d898e13b41a2a7a0
KevRiver <kanggakky1214@gmail.com>
2023-03-21 22:20:17 +0900

    Feat: 사이드바에서 현재 어떤 페이지(탭)을 선택했는지 상태로 관리

--- frontend/churros/src/components/sidebar/LikedArticleTab.js
+++ frontend/churros/src/components/sidebar/LikedArticleTab.js
@@ -1,16 +1,22 @@
-import SidebarItem from "./SidebarItem";
+import SelectableSidebarItem from "./SelectableSidebarItem";
 import { SIDEBAR_ITEM_SIZE } from "./constants";
 import { FaHeart } from "react-icons/fa/index.esm";
 
-const LikedArticleTab = (props) => {
+const LikedArticleTab = ({ itemId }) => {
+  const handleLikedArticleTabClicked = () => {
+    console.log("liked article tab clicked");
+  };
+
   return (
-    <SidebarItem
-      className={`${SIDEBAR_ITEM_SIZE.sm} bg-transparent hover:bg-stone-200`}
+    <SelectableSidebarItem
+      itemId={itemId}
+      className={`${SIDEBAR_ITEM_SIZE.sm} bg-transparent hover:bg-stone-300`}
+      onClick={handleLikedArticleTabClicked}
     >
       <div className="block w-7" />
       <FaHeart className="p-2" size={35} style={{ color: "red" }} />
       <p className="text-sm text-center">좋아요한 기사</p>
-    </SidebarItem>
+    </SelectableSidebarItem>
   );
 };
 
--- frontend/churros/src/components/sidebar/ScrapFolderTab.js
+++ frontend/churros/src/components/sidebar/ScrapFolderTab.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Fragment } from "react";
+import { useSetRecoilState } from "recoil";
 import SidebarItem from "./SidebarItem";
 import {
   IoChevronForwardOutline,
@@ -7,42 +7,47 @@ import {
   IoAddOutline,
 } from "react-icons/io5";
 import { SIDEBAR_ITEM_SIZE } from "./constants";
-import ScrapFolderListItem from "./ScrapFolderListItem";
+import { showScrapFolderListState } from "../../store/sidebar";
 
-const ScrapFolderTab = ({key, scrapFolderListHandler}) => {
+const ScrapFolderTab = () => {
+  const setShowScrapFolderList = useSetRecoilState(showScrapFolderListState);
+
+  const toggleScrapFolderList = () => {
+    setShowScrapFolderList((prev) => !prev);
+  };
 
   const addNewScrapFolder = () => {
     console.log("add new scrap folder");
   };
 
   return (
-      <SidebarItem className={`${SIDEBAR_ITEM_SIZE.sm}`}>
-        <div className="w-full h-full flex flex-row justify-between items-center">
-          <div
-            className={`flex flex-row justify-start items-center w-full hover:bg-stone-200`}
-            onClick={scrapFolderListHandler}
-          >
-            <IoChevronForwardOutline
-              className="w-7 pl-2"
-              size={30}
-              style={{ color: "837F79" }}
-            />
-            <div className="flex flex-row justify-start items-center">
-              <IoFolderSharp
-                className="p-2 hover:bg-stone-200"
-                size={35}
-                style={{ color: "FFB240" }}
-              />
-              <p className="text-sm text-center">스크랩 폴더</p>
-            </div>
-          </div>
-          <IoAddOutline
-            className="w-10 h-full p-2 hover:bg-stone-200"
+    <SidebarItem className={`${SIDEBAR_ITEM_SIZE.sm}`}>
+      <div className="w-full h-full flex flex-row justify-between items-center">
+        <div
+          className={`flex flex-row justify-start items-center w-full hover:bg-stone-300`}
+          onClick={toggleScrapFolderList}
+        >
+          <IoChevronForwardOutline
+            className="w-7 pl-2"
+            size={30}
             style={{ color: "837F79" }}
-            onClick={addNewScrapFolder}
           />
+          <div className="flex flex-row justify-start items-center">
+            <IoFolderSharp
+              className="p-2 hover:bg-stone-200"
+              size={35}
+              style={{ color: "FFB240" }}
+            />
+            <p className="text-sm text-center">스크랩 폴더</p>
+          </div>
         </div>
-      </SidebarItem>
+        <IoAddOutline
+          className="w-10 h-full p-2 hover:bg-stone-300"
+          style={{ color: "837F79" }}
+          onClick={addNewScrapFolder}
+        />
+      </div>
+    </SidebarItem>
   );
 };
 
