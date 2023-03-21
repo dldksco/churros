7a0e7666208314ee8903ba345d9500dab2c8a524
KevRiver <kanggakky1214@gmail.com>
2023-03-21 17:42:19 +0900

    Feat: MainPage 레이아웃 마킹

--- frontend/churros/src/pages/MainPage.js
+++ frontend/churros/src/pages/MainPage.js
@@ -1,13 +1,21 @@
 import React, { Fragment } from "react";
-import { ReactDOM } from "react";
-import { IconContext } from "react-icons";
-import { FaHeart } from "react-icons/fa";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const MainPage = () => {
   return (
     <div className="w-screen h-screen flex flex-row justify-start bg-stone-50">
-      <Sidebar className={"w-72 h-full bg-stone-100"} />
+      <Sidebar/>
+      <div className="flex flex-col justify-start w-full h-full">
+        <div className="flex flex-row justify-between items-center h-14">
+          <h1>This is</h1>
+          <h1>Navbar</h1>
+        </div>
+        <div className="flex flex-col justify-start w-full h-full">
+          <h1>This</h1>
+          <h1>is</h1>
+          <h1>a main section</h1>
+        </div>
+      </div>
     </div>
   );
 };
