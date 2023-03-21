d140ed18fb310f4f98b4f9c791b3d449eb478184
KevRiver <kanggakky1214@gmail.com>
2023-03-21 22:20:17 +0900

    Feat: 사이드바에서 현재 어떤 페이지(탭)을 선택했는지 상태로 관리

--- frontend/churros/src/components/sidebar/MainArticleTab.js
+++ frontend/churros/src/components/sidebar/MainArticleTab.js
@@ -1,16 +1,22 @@
-import SidebarItem from "./SidebarItem";
 import { SIDEBAR_ITEM_SIZE } from "./constants";
 import { IoBookOutline } from "react-icons/io5";
+import SelectableSidebarItem from "./SelectableSidebarItem";
 
-const MainArticleTab = (props) => {
+const MainArticleTab = ({ itemId }) => {
+  const handleMainArticleTabClicked = () => {
+    console.log("main article tab clicked");
+  };
+
   return (
-    <SidebarItem
-      className={`${SIDEBAR_ITEM_SIZE.sm} bg-transparent hover:bg-stone-200`}
+    <SelectableSidebarItem
+      itemId = {itemId}
+      className={`${SIDEBAR_ITEM_SIZE.sm} bg-transparent hover:bg-stone-300`}
+      onClick={handleMainArticleTabClicked}
     >
       <div className="block w-7" />
       <IoBookOutline className="p-2" size={35} style={{ color: "837F79" }} />
       <p className="text-sm text-center">추천 기사</p>
-    </SidebarItem>
+    </SelectableSidebarItem>
   );
 };
 
--- frontend/churros/src/components/sidebar/ScrapFolderListItem.js
+++ frontend/churros/src/components/sidebar/ScrapFolderListItem.js
@@ -1,28 +1,36 @@
-import SidebarItem from "./SidebarItem";
+// constants
 import { SIDEBAR_ITEM_SIZE } from "./constants";
+// icons
 import { IoEllipsisHorizontalSharp } from "react-icons/io5";
 import { FaMinus } from "react-icons/fa";
+// components
+import SelectableSidebarItem from "./SelectableSidebarItem";
 
-const ScrapFolderListItem = ({ title, folderIdx }) => {
+const ScrapFolderListItem = ({ title, itemId, folderIdx }) => {
   const loadScrapFolderPage = () => {
     console.log(`request items of pScrapFolder(${folderIdx})`);
   };
 
-  const showActionMenuModal = () => {
+  const showActionMenuModal = (event) => {
+    event.stopPropagation();
     console.log(`show action menu modal of ScrapFolder(${folderIdx})`);
   };
 
   return (
-    <SidebarItem className={`${SIDEBAR_ITEM_SIZE.sm}`}>
+    <SelectableSidebarItem
+      itemId={itemId}
+      className={`${SIDEBAR_ITEM_SIZE.sm}`}
+      onClick={loadScrapFolderPage}
+    >
       <div className="w-full h-full flex flex-row justify-between items-center">
         <div
-          className={`flex flex-row justify-start items-center w-full h-full hover:bg-stone-200`}
+          className={`flex flex-row justify-start items-center w-full h-full hover:bg-stone-300`}
           onClick={loadScrapFolderPage}
         >
           <div className="block w-7" />
           <div className="flex flex-row justify-start items-center">
             <FaMinus
-              className="p-2 hover:bg-stone-200"
+              className="p-2"
               size={25}
               style={{ color: "837F79" }}
             />
@@ -30,12 +38,12 @@ const ScrapFolderListItem = ({ title, folderIdx }) => {
           </div>
         </div>
         <IoEllipsisHorizontalSharp
-          className="w-10 h-full p-2 hover:bg-stone-200"
+          className="w-10 h-full p-2 hover:bg-stone-300"
           style={{ color: "837F79" }}
           onClick={showActionMenuModal}
         />
       </div>
-    </SidebarItem>
+    </SelectableSidebarItem>
   );
 };
 
