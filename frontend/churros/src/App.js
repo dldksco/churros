9921ef7213c5ba48406915eb1c31cfa0476adefb
KevRiver <kanggakky1214@gmail.com>
2023-03-22 17:02:54 +0900

    Feat: 중첩 라우트 적용

--- frontend/churros/src/App.js
+++ frontend/churros/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import MainPage from './pages/MainPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/main" element={<MainPage />} />
+        <Route path="/news" element={<MainPage />}>
+          <Route path="feed" element={<h1>추천 기사</h1>} />
+          <Route path="likes" element={<h1>좋아요한 기사</h1>} />
+          <Route path="scraps/:idx" element={<h1>스크랩 폴더</h1>} />
+          <Route path="search" element={<h1>검색 페이지</h1>} />
+        </Route>
       </Routes>
     </Router>
   );
