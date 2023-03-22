bc721d28f8a8ed9983d347c84469a52cf30f68cd
윤소희 <elvmeldj@naver.com>
2023-03-22 14:27:57 +0900

    Feat: LikedArticle, DislikedArticle ORM 맵핑 추가

--- recommend/pyrecommend-server/app/models/models.py
+++ recommend/pyrecommend-server/app/models/models.py
@@ -22,3 +22,19 @@ class ReadArticle(Base):
     article_idx = Column(Integer)
     read_date = Column(Date)
     valid_date = Column(Date)
+
+class LikedArticle(Base):
+    __tablename__ = "LikedArticle"
+
+    likes_idx = Column(Integer, primary_key=True, index=True)
+    user_idx = Column(Integer, ForeignKey('User.user_idx'))
+    user = relationship("User", backref=backref("likes", order_by=likes_idx))
+    article_idx = Column(Integer)
+
+class DislikedArticle(Base):
+    __tablename__ = "DislikedArticle"
+
+    dislikes_idx = Column(Integer, primary_key=True, index=True)
+    user_idx = Column(Integer, ForeignKey('User.user_idx'))
+    user = relationship("User", backref=backref("dislikes", order_by=dislikes_idx))
+    article_idx = Column(Integer)
