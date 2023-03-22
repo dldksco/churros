49d2bd709d3ce03b58c6459266f809e3e8aa6cda
kin0308@naver.com <kin0308@naver.com>
2023-03-22 15:14:50 +0900

    Feat : News Api (without article Object call)

--- backend/src/main/java/com/a503/churros/controller/news/NewsController.java
+++ backend/src/main/java/com/a503/churros/controller/news/NewsController.java
@@ -5,9 +5,7 @@ import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -32,10 +30,92 @@ public class NewsController {
         Map<String, Object> resultMap = new HashMap<String, Object>();
 
         // 토큰을 통해 유저 인덱스를 가져오기 - 구현 전
-        List<String> list = ns.sendRecommend(1);
+        List<String> list = ns.sendRecommend(userId);
 
+        resultMap.put("articles", list);
         resultMap.put("result", SUCCESS);
 
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+
+    @GetMapping("/{articleId}")
+    public ResponseEntity<?> getNewsArti(
+            @PathVariable(value = "articleId") long articleId
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        // mongoDb와 연결해서 기사 정보를 가져오기
+        // DTO 와 ENTITY 만들기
+
+        resultMap.put("result", SUCCESS);
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+
+    @GetMapping("/sample")
+    public ResponseEntity<?> getNewsSample(
+//            String token
+            long userId,
+            long articleId
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        List<String> list = ns.sendSample();
+        resultMap.put("result", SUCCESS);
+        resultMap.put("articles", list);
+
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+
+    @PutMapping("/read")
+    public ResponseEntity<?> putRead(
+//            String token
+            long userId,
+            long articleId
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        ns.saveReadArti(userId , articleId);
+        resultMap.put("result", SUCCESS);
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+
+    @PostMapping("/like")
+    public ResponseEntity<?> postLike(
+//            String token
+            long userId,
+            long articleId,
+            long like
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        ns.recordLike(userId , articleId , like);
+        resultMap.put("result", SUCCESS);
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+
+    @GetMapping("/like")
+    public ResponseEntity<?> getLike(
+//            String token
+            long userId
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        List<Long> list = ns.getLikeList(userId);
+
+        resultMap.put("articles" , list);
+        resultMap.put("result", SUCCESS);
+        return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
+    }
+    @PostMapping("/dislike")
+    public ResponseEntity<?> postDisLike(
+//            String token
+            long userId,
+            long articleId
+    ){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        ns.recordDisLike(userId , articleId);
+        resultMap.put("result", SUCCESS);
         return new ResponseEntity<Map<String, Object>>(resultMap , HttpStatus.OK);
     }
 
