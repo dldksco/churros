f06b72bf7abef0241a151d631f86876a80f4ebb2
KevRiver <kanggakky1214@gmail.com>
2023-03-23 09:52:12 +0900

    Feat: 사이드바 아이템 NavLink로 변경 중 스크랩 폴더 리스트 아이템 링크 클릭시 이상한 곳으로 렌더링 되는 현상 발생

--- frontend/churros/src/components/sidebar/SelectableSidebarItem.js
+++ frontend/churros/src/components/sidebar/SelectableSidebarItem.js
@@ -1,14 +1,15 @@
+import { NavLink } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { selectedSidebarItemIdState } from "../../store/sidebar";
 
-const SelectableSidebarItem = ({ itemId, className, children, onClick }) => {
+const SelectableSidebarItem = ({ navigateTo, itemId, className, children, onClick }) => {
   const [selectedSidebarItemId, setSelectedSidebarItemId] = useRecoilState(
     selectedSidebarItemIdState
   );
 
   const handleItemClick = () => {
     setSelectedSidebarItemId((prevItemId) => {
-      if ((prevItemId != itemId) && onClick) {
+      if (prevItemId != itemId && onClick) {
         onClick();
       }
       return itemId;
@@ -16,14 +17,12 @@ const SelectableSidebarItem = ({ itemId, className, children, onClick }) => {
   };
 
   return (
-    <div
-      className={`flex flex-row justify-start items-center w-full ${className} ${
-        selectedSidebarItemId === itemId && "bg-stone-300"
-      }`}
-      onClick={handleItemClick}
+    <NavLink
+      to={navigateTo}
+      className = {({isActive}) => `flex flex-row justify-start items-center w-full ${className} ${isActive ? "bg-stone-300" : ""}`}
     >
       {children}
-    </div>
+    </NavLink>
   );
 };
 
