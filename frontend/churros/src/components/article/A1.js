fdd26b0d143ded2ebed4eca42b02fe11ce34342f
kev <kanggakky1214@gmail.com>
2023-03-27 18:06:48 +0900

    Feat: 메인 페이지 Primary 기사 블록 작업 중

--- frontend/churros/src/components/article/A1.js
+++ frontend/churros/src/components/article/A1.js
@@ -1,15 +1,13 @@
-const A1 = ({articleId, url, src, alt}) => {
+const A1 = ({articleId, title, description, url, src}) => {
     console.log(articleId);
 
     return <a className="flex flex-col w-full h-full justify-start" href={url}>
-        <div className="block w-full h-1/2 justify-center items-center overflow-hidden">
-            <img src={src} alt={alt} className="w-full max-w-md h-auto"/>
+        <div className="relative w-full h-2/3 overflow-hidden">
+            <img className="absolute w-full h-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={src} alt="alt"/>
         </div>
         <div className="flex-1 flex-col justify-start items-center">
-            <p>기사 요약..</p>
-            <p>기사 요약..</p>
-            <p>기사 요약..</p>
-            <p>기사 요약..</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
         </div>
     </a>
 }
--- frontend/churros/src/pages/Main.js
+++ frontend/churros/src/pages/Main.js
@@ -1,13 +1,35 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import test from "../axios-instance/test";
 // import ArticleList from "../components/article/ArticleList";
-import A1 from "../components/article/A1"
+import A1 from "../components/article/A1";
+import test from "../axios-instance/test";
 
 const Main = () => {
+  const [loading, setLoading] = useState(true);
+  const [article, setArticle] = useState({});
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await test.get("/news/1");
+      setArticle(response.data["article"]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex-1 flex-col justify-start w-full h-full p-2 overflow-y-auto">
       <div className="block w-full h-[28rem]">
-        <A1/>
+        {loading && <h1>로딩 중...</h1>}
+        {!loading && <A1 articleId={article["idx"]} url={article["url"]} src={article["imgUrl"]} title={article["title"]} description={article["description"]}/>}
       </div>
       <div className="block w-full h-[14rem]">Article Row</div>
       <div className="block w-full h-[14rem]">Article Row</div>
