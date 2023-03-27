c54d1cbcf2d08e9ef0b92d0c61cf4a24d17a3ef1
Young312886 <hongym0497@gmail.com>
2023-03-27 14:08:11 +0900

    Feat : model training file add

--- recommend/crawling/tokenizer.py
+++ recommend/crawling/tokenizer.py
@@ -1,4 +1,4 @@
-from konlpy.tag import Komoran
+from konlpy.tag import Okt
 import re
 import os
 import pandas as pd
@@ -35,13 +35,12 @@ def tokenstart(startidx, endidx):
     logging.info('Ending my_function')
 
 def token(txt):
-    kom = Komoran()
+    okt = Okt()
     # 아무튼 기사 전문을 변수에 저장
     text = clean(txt)
     text = clean_str(text)
-    word_tokens = kom.pos(text, join="/")
-    stop_words_none = delete_stop_words(word_tokens)
-    result = stemming_text(stop_words_none)
+    word_tokens = okt.pos(text, join="/", stem=True)
+    result = delete_stop_words(word_tokens)
     # 이 결과값이 token된 값
     return result
 
@@ -79,71 +78,11 @@ def clean_str(text):
     return text
 
 
-# 동사 원형 복원
-p1 = re.compile('[가-힣A-Za-z0-9]+/NN. [가-힣A-Za-z0-9]+/XS.')
-p2 = re.compile('[가-힣A-Za-z0-9]+/NN. [가-힣A-Za-z0-9]+/XSA [가-힣A-Za-z0-9]+/VX')
-p3 = re.compile('[가-힣A-Za-z0-9]+/VV')
-p4 = re.compile('[가-힣A-Za-z0-9]+/VX')
-def stemming_text(text):
-    corpus = []
-    for sent in text:
-        ori_sent = sent
-        mached_terms = re.findall(p1, ori_sent)
-        for terms in mached_terms:
-            ori_terms = terms
-            modi_terms = ''
-            for term in terms.split(' '):
-                lemma = term.split('/')[0]
-                tag = term.split('/')[-1]
-                modi_terms += lemma
-            modi_terms += '다/VV'
-            ori_sent = ori_sent.replace(ori_terms, modi_terms)
-        
-        mached_terms = re.findall(p2, ori_sent)
-        for terms in mached_terms:
-            ori_terms = terms
-            modi_terms = ''
-            for term in terms.split(' '):
-                lemma = term.split('/')[0]
-                tag = term.split('/')[-1]
-                if tag != 'VX':
-                    modi_terms += lemma
-            modi_terms += '다/VV'
-            ori_sent = ori_sent.replace(ori_terms, modi_terms)
-
-        mached_terms = re.findall(p3, ori_sent)
-        for terms in mached_terms:
-            ori_terms = terms
-            modi_terms = ''
-            for term in terms.split(' '):
-                lemma = term.split('/')[0]
-                tag = term.split('/')[-1]
-                modi_terms += lemma
-            if '다' != modi_terms[-1]:
-                modi_terms += '다'
-            modi_terms += '/VV'
-            ori_sent = ori_sent.replace(ori_terms, modi_terms)
-
-        mached_terms = re.findall(p4, ori_sent)
-        for terms in mached_terms:
-            ori_terms = terms
-            modi_terms = ''
-            for term in terms.split(' '):
-                lemma = term.split('/')[0]
-                tag = term.split('/')[-1]
-                modi_terms += lemma
-            if '다' != modi_terms[-1]:
-                modi_terms += '다'
-            modi_terms += '/VV'
-            ori_sent = ori_sent.replace(ori_terms, modi_terms)
-        corpus.append((ori_sent))
-    return corpus
-
 # 불용어 제거
 word_file = open("stopwords.txt", "r", encoding="utf-8")
 words = word_file.read()
 stop_words = set(words.split('\n'))
-lemmatization = {"VV","VX","NNG","NNP","NNB","XSV","XSA","SL","XR","SN","SH","SW","MAG","NF"}
+lemmatization = {'Adjective', 'Adverb', 'Alpha', 'Exclamation', 'Foreign', 'Noun', 'Number',  'Unknown', 'Verb'} # 동사와 명사 형용사 및 기타 의미가 존재하는 형태소만을 남김
 def delete_stop_words(text):
     result =  [word for word in text if (not (word.split('/')[0] in stop_words) and word.split('/')[1] in lemmatization)]
     return result
