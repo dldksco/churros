2f352ad8d1d28884993354c0c11ec1fb853dcfb7
kev <kanggakky1214@gmail.com>
2023-03-27 17:12:40 +0900

    Feat: Page 컴포넌트명 변경

---
+++ frontend/churros/src/pages/Main.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import test from "../axios-instance/test";
+import ArticleList from "../components/article/ArticleList";
+
+const Main = () => {
+  return (
+    <div className="flex-1 flex-col justify-start w-full h-full p-2 overflow-y-auto">
+      <div className="block w-full h-[28rem]">Swiper View</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+      <div className="block w-full h-[14rem]">Article Row</div>
+    </div>
+  );
+};
+
+export default Main;
