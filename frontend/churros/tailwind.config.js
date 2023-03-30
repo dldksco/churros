e7982eca9af73e81e337020ae497f3695292aba1
kev <kanggakky1214@gmail.com>
2023-03-30 11:14:49 +0900

    Feat: TailwindCSS custom spacing 추가

--- frontend/churros/tailwind.config.js
+++ frontend/churros/tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js, jsx, ts, tsx}"
-  ],
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
     extend: {
       spacing: {
         "10vh": "10vh",
+        "10%": "10%",
+        "20%": "20%",
+        "25%": "25%",
         "108px": "108px",
         "1080px": "1080px",
-      }
+      },
     },
   },
   plugins: [],
-}
+};
