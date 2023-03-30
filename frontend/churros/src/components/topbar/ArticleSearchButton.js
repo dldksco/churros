e5de73e9b60db2b010fd2cda57fe92b545a3119d
Young312886 <hongym0497@gmail.com>
2023-03-30 17:04:06 +0900

    Feat : 검색 페이지 및 검색 이동 추가
    Former-commit-id: 4a056ceda4dc59a92895e80030f0dfd7282351b6

--- frontend/churros/src/components/topbar/ArticleSearchButton.js
+++ frontend/churros/src/components/topbar/ArticleSearchButton.js
@@ -1,8 +1,8 @@
 import {FaSearch} from "react-icons/fa";
 
-const ArticleSearchButton = (props) => {
+const ArticleSearchButton = ({searchOnClick}) => {
   return (
-    <div className="flex flex-row justify-start items-center w-20 text-lg">
+    <div className="flex flex-row justify-start items-center w-20 text-lg" onClick={searchOnClick}>
       <FaSearch className="mr-2"/>
       <h1 className="font-bold">검색</h1>
     </div>
---
+++ frontend/churros/src/pages/Search.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import LikeArticle from "../components/article/LikeArticle";
+import axios from "axios";
+const LikesPage = () => {
+  const location = useLocation();
+  const searchData = location.state;
+  console.log(searchData);
+  const [searchList, setSearchList] = useState([]);
+
+  useEffect(() => {
+    // 시작과 함께 axios 통신으로 리스트 받아옴
+    setSearchList([
+      { title: "First Article", text: "Text" },
+      { title: "Second Article", text: "Text2" },
+      { title: "Third Article", text: "Text3" },
+      { title: "Fourth Article", text: "Text4" },
+      { title: "Fifth Article", text: "Text5" },
+      { title: "Second Article", text: "Text6" },
+    ]);
+  }, []);
+  return (
+    <div>
+      <div className="text-2xl flex item-center">
+        <p className="font-bold mx-4">{searchData}</p> 에 대한 검색 결과 입니다
+      </div>
+      <div className="grid grid-cols-2 gap-4 p-5">
+        {/* 검색결과 */}
+        {searchList.slice(1).map((article, idx) => (
+          <div key={idx} className="col-span-1 relative">
+            <LikeArticle />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LikesPage;
