b952fcdab1b7a695afc2c646fa69114e33e6cd62
kev <kanggakky1214@gmail.com>
2023-03-31 17:09:57 +0900

    Feat: 더미 데이터 스크랩 모달 완료
    Former-commit-id: 2a11180e2d2c1e6fcbbad9799496a23749e2b964

--- frontend/churros/src/modal/ScrapDialogueModal.js
+++ frontend/churros/src/modal/ScrapDialogueModal.js
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { IoCheckbox, IoSquareOutline } from "react-icons/io5";
 import { api } from "../axios-instance/api";
 
 const ScrapDialogueBackdrop = ({ onClose }) => {
-  return <div className="fixed top-0 left-0 w-full h-screen z-20 bg-black opacity-75" onClick={onClose}/>;
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-screen z-20 bg-black opacity-75"
+      onClick={onClose}
+    />
+  );
 };
 
-const ScrapListItem = ({
-  articleId,
-  folderIdx,
-  folderName,
-  alreadyScrapped,
-}) => {return <div></div>};
-
-const ScrapList = ({ articleId, items }) => {
+const ScrapListItem = ({ articleId, folderIdx, folderName, isScrapped }) => {
   return (
-    <div>
-      {items.map(({ articleId, folderIdx, folderTitle, alreadyScrapped }) => {
+    <div className="flex flex-row w-full h-max my-2">
+      {isScrapped ? (
+        <IoCheckbox size={25} className="text-blue-800" />
+      ) : (
+        <IoSquareOutline size={25} />
+      )}
+      <p
+        className="text-base font-bold px-4"
+        style={{ fontFamily: "Noto Sans KR", fontWeight: 500 }}
+      >{folderName}</p>
+    </div>
+  );
+};
+
+const ScrapList = ({ items }) => {
+  return (
+    <div className="flex flex-col flex-1 overflow-y-auto">
+      {items.map(({ articleId, folderIdx, folderName, isScrapped }) => {
         return (
           <ScrapListItem
             articleId={articleId}
             folderIdx={folderIdx}
-            folderTitle={folderTitle}
-            alreadyScrapped={alreadyScrapped}
+            folderName={folderName}
+            isScrapped={isScrapped}
           />
         );
       })}
@@ -50,7 +65,7 @@ const ScrapDialogueContent = ({ articleId, onClose }) => {
             articleId: articleId,
             folderIdx: folderIdx,
             folderName: folderName,
-            alreadyScrapped: articles?.includes(articleId),
+            isScrapped: articles?.includes(articleId),
           };
         })
       );
@@ -65,19 +80,84 @@ const ScrapDialogueContent = ({ articleId, onClose }) => {
   useEffect(() => {
     // fetchData();
   }, []);
+
+  const dummyItems = [
+    {
+      articleId: 1,
+      folderIdx: 2,
+      folderName: "정치",
+      isScrapped: true,
+    },
+    {
+      articleId: 1,
+      folderIdx: 3,
+      folderName: "시사",
+      isScrapped: false,
+    },
+    {
+      articleId: 1,
+      folderIdx: 2,
+      folderName: "IT",
+      isScrapped: true,
+    },
+    {
+      articleId: 1,
+      folderIdx: 2,
+      folderName: "과학",
+      isScrapped: false,
+    },
+    {
+        articleId: 1,
+        folderIdx: 2,
+        folderName: "정치",
+        isScrapped: true,
+      },
+      {
+        articleId: 1,
+        folderIdx: 3,
+        folderName: "시사",
+        isScrapped: false,
+      },
+      {
+        articleId: 1,
+        folderIdx: 2,
+        folderName: "IT",
+        isScrapped: true,
+      },
+      {
+        articleId: 1,
+        folderIdx: 2,
+        folderName: "과학",
+        isScrapped: false,
+      },
+      {
+        articleId: 1,
+        folderIdx: 2,
+        folderName: "과학",
+        isScrapped: false,
+      },
+      {
+        articleId: 1,
+        folderIdx: 2,
+        folderName: "과학",
+        isScrapped: false,
+      },
+  ];
+
   return (
-    <div className="fixed w-80 h-128 p-2 bg-white top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50">
-      ScrapDialogueContent
+    <div className="fixed flex flex-col w-80 h-128 p-2 bg-white top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50">
+      <p className="w-full h-12 p-2">스크랩하기</p>
+      <ScrapList items={dummyItems}/>
+      <div className="w-full h-12 p-2">새 스크랩 폴더</div>
     </div>
   );
 };
 
 const ScrapDialogueModal = ({ target, onClose }) => {
-
   return (
     <Fragment>
       {ReactDOM.createPortal(
-        <ScrapDialogueBackdrop onClose={onClose}/>,
+        <ScrapDialogueBackdrop onClose={onClose} />,
         document.getElementById("backdrop-root")
       )}
       {ReactDOM.createPortal(
