2558e40ee06cbf2a78edf5c9d6337abde752e498
mandariny <mandariny716@gmail.com>
2023-04-04 00:55:21 +0900

    Feat: 로그 파싱 코드 작성
    Former-commit-id: 2b271dc99c8a95046110574ade534c806f7cf08d

--- recommend/monitoring/main.py
+++ recommend/monitoring/main.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 import datetime
 
-load_dotenv(dotenv_path='../crawling/.env')
+load_dotenv()
 
 # mongoDB 설정
 mongo_host = os.environ.get('MongoHost')
@@ -19,9 +19,12 @@ mongo_client = MongoClient(host=mongo_host, port=mongo_port, username=mongo_user
 db = mongo_client[mongo_db_name]
 collection = db['crawlingLog']
 
+result_json = []
+
 # 로그 파일 읽기
 with open("crawling.log", "r", encoding='utf-8') as file:
     # 파일의 각 줄을 읽어들여 MongoDB에 저장
+
     for line in file:
         # 로그 데이터를 딕셔너리 형태로 저장
         words = line.strip().split('|')
@@ -34,7 +37,7 @@ with open("crawling.log", "r", encoding='utf-8') as file:
         sub_cateogory=''
         running_time = ''
         cnt = ''
-        link = ''
+        # link = ''
         error_msg = ''
         error_msg2 = ''
 
@@ -44,39 +47,44 @@ with open("crawling.log", "r", encoding='utf-8') as file:
             time = datetime.datetime.strptime(date_time[1], '%H:%M:%S,%f')
             level = words[1]
             state = words[2]
-            cagegory = words[3]
+            category = words[3]
 
             if level == 'INFO':
-                if state == 'start':
-                    if cagegory == 'crawling': continue
-                    start_time = datetime.datetime.strptime(words[6], '%H:%M:%S')
-                    running_time = time - start_time
+                if state == 'start' or state == 'finish':
+                    if category != 'tokenize' and category != 'crawling':
+                        start_time = datetime.datetime.strptime(words[4], '%H:%M:%S')
+                        running_time = (time - start_time).total_seconds()
                 elif state == 'success':
-                    cnt = int(words[6])
+                    cnt = int(words[4])
                 elif state == 'fail':
-                    cnt = int(words[6])
+                    cnt = int(words[4])
             elif level == 'ERROR':
                 if state == 'CRAWLING':
-                    sub_cateogory = words[6]
-                    link = words[7]
-                    error_msg = re.findall(r'\[.*?\]', line)[0]
+                    sub_cateogory = words[4]
+                    error_msg = words[5]
                 elif state == 'DB':
-                    sub_cateogory = words[6]
-                    error_msg = words[7]
+                    sub_cateogory = words[4]
+                    error_msg = words[5]
                     if error_msg == 'BulkWriteError':
-                        error_msg2 = re.findall(r'\[.*?\]', line)[0]
-                    else:
-                        error_msg = re.findall(r'\[.*?\]', line)[0]
+                        error_msg2 = words[6]
                 elif state == 'ETC':
-                    pass
+                    error_msg = words[3]
+                    category = ''
 
             result = {'date':date, 'time':time, 'level':level, 'state':state,
                       'category':category, 'sub_cateogory':sub_cateogory, 'running_time':running_time,
-                      'cnt':cnt, 'link':link, 'error_msg':error_msg, 'error_msg2':error_msg2}
-            print(result)
+                      'cnt':cnt, 'error_msg':error_msg, 'error_msg2':error_msg2}
+            # print(result)
+            result_json.append(result)
         except Exception as e:
             print(line)
             print(e)
             print(words)
-        # MongoDB에 데이터 저장
-        # collection.insert_one(log_data)
+    # MongoDB에 데이터 저장
+    # collection.insert_one(log_data)
+    # print(result_json)
+
+try:
+    collection.insert_many(result_json)
+except Exception as e:
+    print(e)
