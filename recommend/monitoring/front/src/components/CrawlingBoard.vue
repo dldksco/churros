07278cd4c4923aaa498e08f49f9035e844e447ab
mandariny <mandariny716@gmail.com>
2023-04-05 03:33:33 +0900

    Chore: 추가 라이브러리 설치
    Former-commit-id: 953736968f089bbb44a882cc7d97d44e41ef328a

--- recommend/monitoring/front/src/components/CrawlingBoard.vue
+++ recommend/monitoring/front/src/components/CrawlingBoard.vue
@@ -6,20 +6,23 @@
     <label for="datepicker_end">검색 종료일</label>
     <b-form-datepicker id="datepicker_end" v-model="end_day" :min="start_day" class="mb-2"></b-form-datepicker>
     <p>Value: '{{ end_day }}'</p>
-    <b-button variant="outline-secondary" @click="getData" >Secondary</b-button>
+    <b-button variant="outline-secondary" @click="getData" >조회하기</b-button>
     <p>Res: {{ result }} </p>
+
   </div>
 </template>
 
 <script>
 import {api} from "../utils/axios"
+
 export default {
     data() {
         const now = new Date()
       return {
         start_day: `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`,
         end_day: `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`,
-        result: ''
+        result: '',
+        
       }
     },
     methods:{
@@ -28,6 +31,7 @@ export default {
             api.success({"start_day" : this.start_day, "end_day" : this.end_day})
             .then((res) =>{
                 this.result = res.data;
+              
             });
             
         }
