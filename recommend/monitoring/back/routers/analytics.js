3888d2ba6fed100ddcf431009261aadf434492bd
윤소희 <elvmeldj@naver.com>
2023-04-04 17:10:48 +0900

    Feat: 뉴스 데이터 스키마 추가
    Former-commit-id: 93aaf55e6d203170ba132e58bcad2fb00240bf8c

--- recommend/monitoring/back/routers/analytics.js
+++ recommend/monitoring/back/routers/analytics.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const connect = require('../db/index');
 const Log = require('../db/logSchema');
+const NewsData = require('../db/dataSchema');
 
 const router = express.Router();
 
 connect();
 
-router.get('/error', async (req, res) => {
+router.get('/success', async (req, res) => {
   try {
     // const logs = await Log.findOne({});
     const logs = await Log.find({ date: { $gte: new Date('2023-04-02') }, level: 'ERROR' });
@@ -19,5 +20,19 @@ router.get('/error', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+router.get('/fail', async (req, res) => {
+  try {
+    // const logs = await Log.findOne({});
+    const news = await NewsData.find({ publish_date: { $gte: new Date('2023-03-28') }, cat1: '연예' });
+    // const logs = await db.collection('crawlingLog').find({ date: { $gte: new Date('2023-04-03') } }).toArray();
+    // const logs = await Log.find({ date: { $gte: new Date('2023-04-03') } });
+    res.json(news);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 module.exports = router;
