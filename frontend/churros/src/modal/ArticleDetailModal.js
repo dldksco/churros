e35de801a1d203c8e9fbef95828c9471c7fbfeb4
Young312886 <hongym0497@gmail.com>
2023-04-05 10:27:02 +0900

    Fix : 디테일 페이지 출력 오류 수정
    Former-commit-id: a07ad0d57b23d683ec95649da9976faec0a2b092

--- frontend/churros/src/modal/ArticleDetailModal.js
+++ frontend/churros/src/modal/ArticleDetailModal.js
@@ -11,7 +11,6 @@ const ArticleDetailBackdrop = ({ hideDetail }) => {
     ></div>
   );
 };
-
 const ArticleDetailContent = ({ url }) => {
   const [htmlObject, setHtmlObject] = useState();
 
@@ -19,54 +18,53 @@ const ArticleDetailContent = ({ url }) => {
   const articleLocation = url.match(regex);
 
   console.log(articleLocation);
-  
-  try {
-    const response = api.get(`/news/call`, {
-      headers: {
-        Accept: "application/json",
-      },
-      params: {
-        url: articleLocation[1],
-      },
-    });
+  const fetchData = async (url) => {
+    try {
+      const response = await api.get(`/news/call`, {
+        params: {
+          url: url,
+        },
+      });
 
-    const htmlContent = response.data["html"];
-    console.log(htmlContent?.slice(0, 100));
+      const htmlContent = response.data["html"];
+      console.log(htmlContent?.slice(0, 100));
 
-    htmlContent.replace(/data-src=/g, "src=");
-    setHtmlObject(
-      <div
-        dangerouslySetInnerHTML={{
-          __html: htmlContent?.replace(/data-src=/g, "src="),
-        }}
-      />
-    );
-  } catch (error) {
-    console.log(error);
-  } finally {
-    const modalHolderStyle = {
-      position: "fixed",
-      top: "10vh",
-      left: "10%",
-      width: "80%",
-      height: "90vh",
-      zIndex: 100,
-      overflow: "hidden",
-    };
-    return (
-      <div style={modalHolderStyle}>
-        <div className="flex flex-col w-full h-full justify-start bg-white">
-          <section className="overflow-y-auto">{htmlObject}</section>
-          <footer>footer</footer>
-        </div>
+      htmlContent.replace(/data-src=/g, "src=");
+      setHtmlObject(
+        <div
+          dangerouslySetInnerHTML={{
+            __html: htmlContent,
+          }}
+        />
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchData(articleLocation[0]);
+
+  const modalHolderStyle = {
+    position: "fixed",
+    top: "10vh",
+    left: "10%",
+    width: "80%",
+    height: "90vh",
+    zIndex: 100,
+    overflow: "hidden",
+  };
+  return (
+    <div style={modalHolderStyle}>
+      <div className="flex flex-col w-full h-full justify-start bg-white">
+        <section className="overflow-y-auto">{htmlObject}</section>
+        <footer>footer</footer>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 const ArticleDetailModal = ({ url, hideDetail }) => {
-  console.log(url)
-  console.log(typeof url)
+  console.log(url);
+  console.log(typeof url);
   return (
     <Fragment>
       {ReactDOM.createPortal(
