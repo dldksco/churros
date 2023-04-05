d30b5f759ecced100cd35197feeb8a4ab95889de
Young312886 <hongym0497@gmail.com>
2023-04-05 12:21:42 +0900

    Design : 버튼 회색으로
    Former-commit-id: 59deab00223f11093260c738092320a8ff11451e

--- frontend/churros/src/modal/SearchModal.js
+++ frontend/churros/src/modal/SearchModal.js
@@ -61,7 +61,7 @@ const SearchContent = ({ searchOnClick, searchOffClick }) => {
             <input
               type="text"
               id="voice-search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
               placeholder="검색어를 입력해 주세요"
               required
               value={searchWord}
@@ -74,7 +74,7 @@ const SearchContent = ({ searchOnClick, searchOffClick }) => {
           </div>
           <button
             type="submit"
-            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-gray-700 rounded-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             onClick={searchGo}
           >
             <svg
