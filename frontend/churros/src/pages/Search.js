2e25de945e0b2fecf7cc916f0a69d19c777e24b6
Young312886 <hongym0497@gmail.com>
2023-04-06 17:31:41 +0900

    Model : Remodleing
    Former-commit-id: 6f73164aa3e1443965749468542c00f11f415a8a

--- frontend/churros/src/pages/Search.js
+++ frontend/churros/src/pages/Search.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import Article from "../components/article/Article";
 import useFetch from "../components/useFetch";
-
+import { api } from "../axios-instance/api";
 const SearchPage = () => {
   const location = useLocation();
   const searchData = location.state;
@@ -11,7 +11,17 @@ const SearchPage = () => {
   const { loading, searchList, last } = useFetch(searchData, pageNum);
   const loader = useRef();
   // 시작과 함께 axios 통신으로 리스트 받아옴
-
+  const [likeList, setLikeList] = useState([]);
+  const likeListGet = async () => {
+    try {
+      const response = await api.get(`/news/like`);
+      const { result, articles } = response.data;
+      setLikeList(articles);
+      console.log(`likes list set ${likeList}: ${result}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const handleObserver = useCallback((entries) => {
     const target = entries[0];
     if (!last) {
@@ -22,6 +32,7 @@ const SearchPage = () => {
   }, []);
 
   useEffect(() => {
+    likeListGet();
     const option = {
       root: null,
       rootMargin: "20px",
@@ -40,7 +51,7 @@ const SearchPage = () => {
         {searchList &&
           searchList.map((article, idx) => (
             <div key={idx} className="col-span-1 relative">
-              <Article shape="2" articleIdx={article.idx} />
+              <Article shape="2" articleIdx={article.idx} likelist={likeList}/>
             </div>
           ))}
         {loading && <p className="col-span-2">Loading...</p>}
