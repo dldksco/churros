559a6bcffa2dd831ca46d2f0b45abb82ab42f9b9
Young312886 <hongym0497@gmail.com>
2023-04-06 23:41:33 +0900

    Fix : useEffect 추가
    Former-commit-id: 83f09debf09e39395081bd69c16c5fb588320646

--- frontend/churros/src/App.js
+++ frontend/churros/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import Main from "./pages/Main"
+import Main from "./pages/Main";
 import Likes from "./pages/Likes";
 import Search from "./pages/Search";
 import Scraps from "./pages/Scraps";
@@ -12,18 +12,22 @@ import KakaoRedirectHandler from "./pages/KakaoRedirectHandler";
 // 6팀 잠시 요청
 import TagManager from "./module";
 
-const tagManager = new TagManager('http://ec2-3-38-85-143.ap-northeast-2.compute.amazonaws.com/api/v1/dump',  "3ee56d05-9119-41dd-b2a3-5325ef06a713", ['click'],"*")
-
+const tagManager = new TagManager(
+  "http://ec2-3-38-85-143.ap-northeast-2.compute.amazonaws.com/api/v1/dump",
+  "3ee56d05-9119-41dd-b2a3-5325ef06a713",
+  ["click"],
+  "*"
+);
 
 function App() {
   // 6팀 잠시 요청
-  const location = useLocation()
+  const location = useLocation();
   useEffect(() => {
-      tagManager.attach();
-      return () => {
-          tagManager.detach();
-      }
-   }, [location])
+    tagManager.attach();
+    return () => {
+      tagManager.detach();
+    };
+  }, [location]);
 
   return (
     <Router>
@@ -35,7 +39,7 @@ function App() {
           <Route path="scraps/:idx" element={<Scraps />} />
           <Route path="search" element={<Search />} />
         </Route>
-        <Route path="/kakao/handler" element={<KakaoRedirectHandler />}/>
+        <Route path="/kakao/handler" element={<KakaoRedirectHandler />} />
       </Routes>
     </Router>
   );
