c4dc582e028849c4454dd93542c24fb84aa01347
Young312886 <hongym0497@gmail.com>
2023-04-06 23:35:15 +0900

    Design : 이모티콘 변경
    Former-commit-id: 347aab09375f0c5d5e9fb004a5174eb793c2bcdc

--- frontend/churros/src/components/common/EmptyPage.js
+++ frontend/churros/src/components/common/EmptyPage.js
@@ -1,30 +1,12 @@
 import React from "react";
+import { GiKnot } from "react-icons/gi";
 
 const EmptyPage = ({ message, className }) => {
   return (
     <div
       className={`flex flex-col justify-center items-center h-full w-full text-gray-500`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-12 w-12 mb-4"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 8a6 6 0 00-6-6 6 6 0 00-6 6v8a2 2 0 002 2h12a2 2 0 002-2V8h0z"
-        />
-      </svg>
+      <GiKnot className="text-3xl text-[#CBA870]"/>
       <h2 className="text-lg font-medium text-center">{message}</h2>
     </div>
   );
