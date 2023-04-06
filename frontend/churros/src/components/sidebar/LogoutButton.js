d0d2171d47da7cbf2cf8909ac052b17d921a9975
kev <kanggakky1214@gmail.com>
2023-04-06 16:54:17 +0900

    Feat: LogoutButton 수정
    Former-commit-id: 5407bbf1e2cc31a4ea34e572f8cc25f24927d3a1

--- frontend/churros/src/components/sidebar/LogoutButton.js
+++ frontend/churros/src/components/sidebar/LogoutButton.js
@@ -2,15 +2,35 @@ import React from "react";
 import { IoLogOutOutline } from "react-icons/io5";
 import { api } from "../../axios-instance/api";
 import { useNavigate } from "react-router-dom";
+import { useResetRecoilState } from "recoil";
+import { userInfoState } from "../../store/user";
+import { accessTokenState, refreshTokenState } from "../../store/auth";
+import { showScrapFolderListState, scrapFolderListState } from "../../store/sidebar-global-state";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+
+  const resetUserInfo = useResetRecoilState(userInfoState);
+  const resetAccessToken = useResetRecoilState(accessTokenState);
+  const resetRefreshToken = useResetRecoilState(refreshTokenState);
+  const resetShowScrapFolderList = useResetRecoilState(showScrapFolderListState);
+  const resetScrapFolderList = useResetRecoilState(scrapFolderListState);
+
+  const logout = () => {
+    resetAccessToken();
+    resetRefreshToken();
+    resetUserInfo();
+    resetShowScrapFolderList();
+    resetScrapFolderList();
+  }
+
   const fetchData = async () => {
     try {
-      const response = await api.post(`/user/logout`,
-      );
+      const response = await api.post(`/user/logout`);
       const { result } = response.data;
       console.log(`logout result : ${result}`);
+      console.log("reset recoil-persist");
+      logout();
       navigate("/landing")
     } catch (error) {
       console.log(error);
